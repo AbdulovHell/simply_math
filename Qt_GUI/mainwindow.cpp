9051d7d464e28fa8355edacdd980e604dae0e4b3
djfox12rus <djfox12rus@gmail.com>
2017-03-12 13:58:56 +0300

    123

--- Qt_GUI/mainwindow.cpp
+++ Qt_GUI/mainwindow.cpp
@@ -16,11 +16,6 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_startButton_clicked(){
-    //var_const pi = var_const("pi", 3.1415926535897932384626433832);
-    //var_const e = var_const("e", 2.71828182846);
-
-    //vector<var_const> general_var_const = { pi,e };
-
     QString str=ui->inputText->toPlainText();
     int size=str.toStdString().size();
     char* input=(char*)malloc(size+1);
@@ -30,5 +25,4 @@ void MainWindow::on_startButton_clicked(){
 
     QString outstr=QString::fromLocal8Bit(output,strlen(output));
     ui->outputText->setText(outstr);
-    //int check = input_to_analize(&general_var_const);
 }

0f2c99cdd149c860c5e040902434daf11b6e80a6
AbdulovHell <dronovova@gmail.com>
2017-03-12 11:59:55 +0300

    123

--- src/IO.h
+++ src/IO.h
@@ -1,9 +1,51 @@
 #include <iostream>
 
 namespace Project {
+	namespace Core {
+
+		const int simple_nums[] = { 2,3,5,7,11,13,17,19,23 };	//size 9
+
+		void reduce(int* u, int* d) {
+			for (int i = 0; i < 9;i++)
+				if (*u%simple_nums[i] == 0 && *d%simple_nums[i] == 0) {
+					*u /= simple_nums[i];
+					*d /= simple_nums[i];
+					reduce(u, d);
+					return;
+				}
+		}
+
+		bool isInteger(double var) {
+			int a = var;
+			if (abs(var - a) < 0.000000001)
+				return true;
+			else
+				return false;
+		}
+
+		int isntPeriodical(double num) {
+			num = GET_FRAC(num);
+			int z = 0;
+			while (true) {
+				
+				if (z > 30) {
+					z = 0;
+					break;
+				}
+			}
+		}
+
+		enum class var_type {
+			FRACTIONAL,	//дробное
+			INTEGER_ROUND,	//целое с округлением
+			INTEGER,	//целая часть
+			TOWER
+		};
+	}
 	namespace IO {
 
 		using namespace std;
+		using namespace Project::Core;
 
 		char* err_str(int pos) {	//составляет строку с указателем до неизвестного символа
 			char* str = (char*)malloc(pos + 1);
@@ -52,21 +94,6 @@ namespace Project {
 				return NULL;
 		}
 
-		bool isInteger(double var) {
-			int a = var;
-			if (abs(var - a) < 0.000000001)
-				return true;
-			else
-				return false;
-		}
-
-		enum class var_type {
-			FRACTIONAL,	//дробное
-			INTEGER_ROUND,	//целое с округлением
-			INTEGER,	//целая часть
-			TOWER
-		};
-
 		string to_string(double var, var_type type, int decimals) {	//переменная, как представить, количество знаков после запятой(пока от 0 до 9)
 			string sOut;
 			char buf[25];
@@ -76,7 +103,7 @@ namespace Project {
 
 			switch (type) {
 			case var_type::FRACTIONAL:
-				outFormat[2] = 48+decimals;
+				outFormat[2] = 48 + decimals;
 				sprintf(buf, outFormat, var);
 				sOut = buf;
 				break;
@@ -93,6 +120,7 @@ namespace Project {
 			}
 			return sOut;
 		}
+
 		char* to_char_string(double var, var_type type, int decimals) {	//переменная, как представить, количество знаков после запятой(пока от 0 до 9)
 			char buf[25];
 			int temp;

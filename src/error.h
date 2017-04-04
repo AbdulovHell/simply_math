bff31f944b47ec3182b721c74cee406f415750fa
AbdulovHell <dronovova@gmail.com>
2017-04-04 13:39:22 +0300

    error.h

---
+++ src/error.h
@@ -0,0 +1,31 @@
+namespace Project {
+	using namespace std;
+
+	public class ProjectError {
+
+	public:
+		enum class ErrorCode : int {
+			ERROR1 = 1,
+			ERROR2 = 2
+		};
+
+		static wstring GetProjectError(ErrorCode code) {
+			switch (code)
+			{
+			case ErrorCode::ERROR1:
+				return L"Error1";
+			case ErrorCode::ERROR2:
+				return L"Error2";
+			default:
+				return L"Unresolved error";
+			}
+		}
+
+	private:
+		struct _ErrorPresent {
+			wstring textPresent;
+			ErrorCode code;
+		};
+
+	};
+}

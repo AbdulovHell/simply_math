78a3c14239b412555e3c0b126f0fd40f38d41020
AbdulovHell <dronovova@gmail.com>
2017-05-15 18:56:34 +0300

    Многострочный ввод Qt

--- Qt_GUI/mainwindow.h
+++ Qt_GUI/mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    bool isOutOrEmpty(wchar_t*,size_t);
 
 private slots:
     void Calc();
